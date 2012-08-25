8cd81a97eb2a16f52c62791a87573d27a238e454
Andrew Conner <git@andrewconner.org>
2012-08-25 16:04:26 -0400

    Clarified routes and added README.md.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Sample Play! 2.0.3 Application with Angular.js integration.
+
+To install, install `sbt`, and clone this repository. Then, run `sbt run` in the project directory. By default, the server listens on `http://localhost:9000`.
--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -7,11 +7,11 @@ object Application extends Controller {
   
   // For server-side defined routes that hand off rending to
   // angular.js
-  def angularApp = Action {
+  def angularApp(param: String = "") = Action {
     Ok(views.html.angular())
   }
-  def index = angularApp
-  def view1 = angularApp
-  def view2 = angularApp
+  def index = angularApp()
+  def view1 = angularApp()
+  def view2 = angularApp()
 
 }
